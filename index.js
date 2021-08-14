f8e328c46082309146ea57f3cee27710eb4d9180
Lauren <laurenb252@outlook.com>
2021-08-14 13:47:07 +0100

    Fetches quote from database and posts in channel
    Implemented functionality where user can type '!quote' command, and the bot posts a random quote in the channel using a randomly-generated index. Will test with more quotes in DB.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// require the needed discord.js classes
+// Require the needed discord.js classes
 const { Client, Intents, Collection } = require('discord.js');
 require('dotenv').config();
 const mongoose = require('mongoose');
 const Quotes = require('./schemas/quote-schema');
 
-// create a new Discord client
+// Create a new Discord client
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 client.commands = new Collection();
 client.events = new Collection();
@@ -19,18 +19,24 @@ mongoose.connect(process.env.MONGODB_SRV, {
     console.log(err);
 });
 
-// when the client is ready, run this code
-// this event will only trigger one time after logging in
+// When the client is ready, run this code
+// This event will only trigger one time after logging in
 client.once('ready', () => {
     console.log('Frankie is online!');
     client.user.setActivity('The Bachelor', { type: 'WATCHING' });
 });
 
+// Message event
 client.on('messageCreate', async (message) => {
-    const newQuote = await Quotes.create({
-        quote: message
-    });
+    // If user types quote command
+    if (message.content.toLowerCase() === "!quote") {
+        // Generates a random number
+        const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+        // Fetches all entries in the DB
+        const quote = await Quotes.find();
+        // Posts a quote to database using randomly generated index
+        message.channel.send(quote[random(0, 2)].quote);
+    }
 });
-
 // login to Discord with your app's token
 client.login(process.env.TOKEN);
